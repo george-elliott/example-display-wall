072e094639b938133230b728d565e89839a6706b
geehaws <george.haws@getchute.com>
2014-02-03 09:55:53 -0800

    define asset view for tweets, customize wallview

--- src/js/tileview.js
+++ src/js/tileview.js
@@ -2,27 +2,15 @@ define(
   [
     'core/assetview',
     'hbs!src/templates/asset',
-    'hbs!src/templates/tweet',
     'src/js/assetlightboxview'
   ], function(
     AssetView,
     AssetTemplate,
-    TweetTemplate,
     AssetLightboxView
   ) {
 
   // White Decent Asset View, Go!
   return AssetView.extend({
-    lightboxViewType: AssetLightboxView,
-    initialize: function() {
-      AssetView.prototype.initialize.apply(this, arguments);
-      this.template = (this.model.get('tweet') === "tweet") ? AssetTemplate : TweetTemplate;
-    },
-    onRender: function() {
-      var view = this;
-      view.$('img.chute-user-avatar').on('load error', function() {
-        view.triggerMethod('load:success', view);
-      });
-    }
+    lightboxViewType: AssetLightboxView
   });
 });
