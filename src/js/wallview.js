8e7cd077fc7bbf972964052cb4e164a98e1696d2
gregeng <greg.eng@gmail.com>
2014-06-03 10:23:28 -0700

    add script to the onItemviewsLoad method and call masonry layout again there

--- src/js/wallview.js
+++ src/js/wallview.js
@@ -3,12 +3,14 @@ define(
     'underscore',
     'src/js/tileview',
     'src/js/tweetview',
-    'components/wall'
+    'components/wall',
+    'masonry'
   ], function(
     _,
     AssetView,
     TweetView,
-    WallView
+    WallView,
+    Masonry
   ) {
 
     return WallView.extend({
@@ -22,6 +24,33 @@ define(
         } else {
           return new AssetView(options);
         }
+      },
+      onItemviewsLoad: function(views) {
+        var collectionView = this;
+        if (this.enable.masonry) {
+          if (this.masonry) {
+            _.each(views, function(view) {
+              collectionView.masonry.appended(view.el);
+              view.appended = true;
+            });
+          } else {
+            this.masonry = new Masonry(this.el, {
+              gutter: this.itemViewOptions.margin,
+              columnWidth: this.itemViewOptions.width,
+              itemSelector: '.chute-asset-view',
+              isFitWidth: true,
+              hiddenStyle: {}
+            });
+
+          }
+        }
+        this.$el.removeClass('chute-component-loading');
+        _.delay(function() {
+          collectionView.$el.children().removeClass('chute-loaded');
+        }, 1000);
+
+        this.$el.after('<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>');
+        this.masonry.layout();
       }
     });
 });
